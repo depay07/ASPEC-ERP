c05c5b3a878a69aa4e58ceabf20710cd508a0810
depay07 <60849099+depay07@users.noreply.github.com>
2026-03-22 15:05:59 +0900

    Update router.js

--- js/router.js
+++ js/router.js
@@ -218,6 +218,7 @@ function getTableStructure(tab) {
         bookkeeping: '<thead><tr><th style="width:10%">일자</th><th style="width:12%">계정과목</th><th style="width:20%">사용처(적요)</th><th style="width:10%">결제수단</th><th style="width:12%">금액</th><th>비고</th><th style="width:15%">관리</th></tr></thead>',
         meeting_logs: '<thead><tr><th style="width:12%">미팅날짜</th><th style="width:15%">업체명</th><th style="width:15%">참석자</th><th>미팅 내용 (요약)</th><th style="width:20%">향후 계획</th><th style="width:10%">관리</th></tr></thead>',
         cost_management: '<thead><tr><th style="width:10%">주문일자</th><th style="width:15%">거래처</th><th style="width:20%">주문명(대표품목)</th><th style="width:12%">총 매출액</th><th style="width:12%">총 원가(입력)</th><th style="width:12%">마진금액</th><th style="width:8%">마진율</th><th style="width:10%">관리</th></tr></thead>',            
+        projects: '<thead><tr><th style="width:15%">프로젝트명</th><th style="width:10%">고객사</th><th style="width:8%">상태</th><th style="width:15%">진척도</th><th style="width:8%">담당자</th><th style="width:10%">검사 종류</th><th style="width:15%">광학 조건</th><th>비고</th><th style="width:8%">관리</th></tr></thead>', 
         sales: '<thead><tr><th style="width:10%">일자</th><th style="width:15%">거래처</th><th style="width:10%">담당자</th><th style="width:10%">공급가액</th><th style="width:10%">부가세</th><th style="width:10%">합계</th><th style="width:15%">비고</th><th style="width:8%">계산서</th><th style="width:12%">관리</th></tr></thead>'
     };
     
